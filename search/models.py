99d576af3c4b447d7a17a1a5b7ecfb9d2ca2ad6d
Nick Hirakawa <nh0815@gmail.com>
2014-07-30 23:16:37 -0400

    Added db models

--- search/models.py
+++ search/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Word(models.Model):
+	word = models.CharField(max_length=30)
+
+
+class Document(models.Model):
+	docid = models.IntegerField(default=-1)
+
+
+class Entry(models.Model):
+	word = models.ForeignKey('Word', primary_key=True)
+	docid = models.ForeignKey('Document', primary_key=True)
+	freq = models.IntegerField(default=0)
