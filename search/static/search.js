bb605bb638adbd76589f07a6585e90e151322c23
Nick Hirakawa <nh0815@gmail.com>
2014-08-04 23:18:53 -0400

    Parse results and sort by score

--- search/static/search.js
+++ search/static/search.js
@@ -2,8 +2,20 @@ $(document).ready(function(){
 	$('#form').submit(function(e){
 		e.preventDefault();
 		var query = $('#query').val();
+		var docs = [];
 		$.get('/search/query/', {query: query}, function(data){
-			console.log(data);
+			var result = JSON.parse(data);
+			console.log(typeof result);
+			for(var key in result){
+				var obj = {};
+				obj['doc'] = key;
+				obj['score'] = result[key]
+				docs.push(obj)
+			}
+			docs.sort(function(a, b){
+				return b.score-a.score;
+			});
+			console.log(docs);
 		});
 	});
 });
