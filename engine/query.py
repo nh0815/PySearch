c47b690528debd6e655c03579bd4c76b236f7e64
Nick Hirakawa <nh0815@gmail.com>
2014-08-04 21:42:19 -0400

    Query processor scores documents from file instead of database

--- engine/query.py
+++ engine/query.py
@@ -1,7 +1,6 @@
 __author__ = 'Nick Hirakawa'
 
 
-from db import CouchDBHandler
 from invdx import build_data_structures
 from rank import *
 from collections import OrderedDict
@@ -16,7 +15,6 @@ class QueryProcessor:
 		self.dlt = dlt
 		self.ft = ft
 		self.score_function = score_function
-		self.db = CouchDBHandler()
 
 	def run(self):
 		results = []
@@ -31,39 +29,43 @@ class QueryProcessor:
 		return results
 
 	def run_query_likelihood(self, query):
-		mu = 2000
 		print 'query:', query
 		query_result = OrderedDict()  # collect document rankings for each value of mu
-		print "running query with mu=%d" % mu
-		mu_result = OrderedDict()  # collect document rankings for this value of mu
-		for term in query:
-			print 'searching index'
-			index = self.db.get(term)
-			if index:  # check if word is in index
-				print 'found word in index:', index['_id']
-				print 'scoring documents'
-				docs = set()
-				#score documents that contain term
-				print index['index']
-				for docid, f in index['index'].iteritems():
-					docs.add(docid)
-					score = score_query_likelihood(f=float(f), mu=mu, c=self.ft.get_frequency(term), C=len(self.ft), D=len(self.dlt))
-					if docid in mu_result:
-						mu_result[docid] += score  # document has partial score
-					else:
-						mu_result[docid] = score  # document has no partial score
+		for mu in mu_values:
+			print "running query with mu=%d" % mu
+			mu_result = OrderedDict()  # collect document rankings for this value of mu
+			for term in query:
+				with open(self.idx_file, buffering=1000000000) as idx:
+					print 'searching index'
+					for line in idx:
+						tmp = line.split()
+						word = tmp.pop(0)
+						freq = [tuple(x.split('-')) for x in tmp]
+						if term == word:
+							print 'found word in index:', word
+							docs = set()
 
-				# score documents that don't contain term
-				print 'scoring other documents'
-				tmp = [str(x) for x in range(len(self.dlt))]
-				s = set(tmp).difference(docs)
-				score = score_query_likelihood(f=0, mu=mu, c=self.ft.get_frequency(term), C=len(self.ft), D=len(self.dlt))
-				for docid in s:
-					if docid in mu_result:
-						mu_result[docid] += score
-					else:
-						mu_result[docid] = score
-				break
+							print 'scoring documents'
+							#score documents that contain term
+							for docid, f in freq:
+								docs.add(docid)
+								score = score_query_likelihood(f=float(f), mu=mu, c=self.ft.get_frequency(term), C=len(self.ft), D=len(self.dlt))
+								if docid in mu_result:
+									mu_result[docid] += score
+								else:
+									mu_result[docid] = score
+
+							print 'scoring other documents'
+							#score documents that don't contain term
+							tmp = [str(x) for x in range(len(self.dlt))]
+							s = set(tmp).difference(docs)
+							score = score_query_likelihood(f=0, mu=mu, c=self.ft.get_frequency(term), C=len(self.ft), D=len(self.dlt))
+							for docid in s:
+								if docid in mu_result:
+									mu_result[docid] += score
+								else:
+									mu_result[docid] = score
+							break
 			query_result[mu] = mu_result
 		return query_result
 
