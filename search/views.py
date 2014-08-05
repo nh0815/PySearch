c288232671834920e661d743883d18c1b7f9723a
Nick Hirakawa <nh0815@gmail.com>
2014-08-04 23:09:03 -0400

    Extract constructor expressions

--- search/views.py
+++ search/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from json import dumps
+from engine.query import QueryProcessor
+from engine.invdx import InvertedIndex, DocumentLengthTable, WordFrequencyTable
+import os
 
 # Create your views here.
+
+dlt = DocumentLengthTable()
+dlt.read('index/length.txt')
+wft = WordFrequencyTable()
+wft.read('index/freq.txt')
+proc = QueryProcessor('index/index.txt', dlt, wft)
 
 def index(request):
 	return render(request, 'search/index.html', {})
 
 def query(request):
-	query = request.GET.get('query')
-	return HttpResponse(query)
+	query = request.GET.get('query').split(' ')
+	results = proc.run_query_likelihood(query)
+	print results
+	return HttpResponse(dumps(query))
