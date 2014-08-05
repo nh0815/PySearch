3986a9fdcbe7b83265e2ebed32d7a7310f6b4519
Nick Hirakawa <nh0815@gmail.com>
2014-08-04 22:32:04 -0400

    Added root url route

--- pysearch/urls.py
+++ pysearch/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
+import views
 
 from django.contrib import admin
 admin.autodiscover()
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pysearch.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+	url(r'^$', views.index, name='index'),
 	url(r'^search/', include('search.urls')),
 )
