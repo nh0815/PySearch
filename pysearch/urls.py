bdcca9f505c185fa0ade4e93a88b8dabc85f9176
Nick Hirakawa <nh0815@gmail.com>
2014-08-04 22:23:19 -0400

    Remove access to admin site

--- pysearch/urls.py
+++ pysearch/urls.py
@@ -7,7 +7,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pysearch.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
 	url(r'^search/', include('search.urls')),
 )
